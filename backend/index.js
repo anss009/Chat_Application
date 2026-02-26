2f58332c3bccd3e5b66e1c3385dd8d7ebee00f67
Muhammad Anss <ansslucky07@gmail.com>
2026-02-27 00:14:34 +0200

    .

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser())
 const corsOptions = {
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "https://chat-application-taupe-ten.vercel.app"],
     credentials: true,
 }
 app.use(cors(corsOptions));
